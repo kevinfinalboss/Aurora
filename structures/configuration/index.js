2e4b08af46deb2d85d561773b3130e9931c7e8a5
Kevin the GOAT <kevinmg50@gmail.com>
2024-08-20 21:26:00 -0300

    refactor: refatorado dados de configuração

--- structures/configuration/index.js
+++ structures/configuration/index.js
@@ -1,14 +1,11 @@
-module.exports = {
-    client_token: "MTI1NTYwNTQyNTUwODk3NDY1Mw.GpX09Q.o44yeVZ7k7KyGOJwwBbndy-wicT1wEtllY6FQk",
-    client_id: "1255605425508974653",
-    client_prefix: "gomes!",
-    mongodb_url: "", //optional
-    developers: ["786504767358238720"],
-    sharding: true,
-    database: false,
-}
+require('dotenv').config();
 
-/**
- * Get discord bot token from here https://discord.com/developers/applications
- * Get mongodb url from https://www.mongodb.com/
- */
+module.exports = {
+    client_token: process.env.CLIENT_TOKEN,
+    client_id: process.env.CLIENT_ID,
+    client_prefix: process.env.CLIENT_PREFIX,
+    mongodb_url: process.env.MONGODB_URL,
+    developers: process.env.DEVELOPERS ? process.env.DEVELOPERS.split(',') : [],
+    sharding: process.env.SHARDING === 'true',
+    database: process.env.DATABASE === 'true',
+}
