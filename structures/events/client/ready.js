36ca36c76c8bf4cebe5c3e3489950b2c33a5b646
Kevin the GOAT <kevinmg50@gmail.com>
2024-08-21 09:28:37 -0300

    fix: corrigido a presence quando o bot liga

--- structures/events/client/ready.js
+++ structures/events/client/ready.js
@@ -2,18 +2,33 @@ const { ActivityType } = require("discord.js");
 const client = require("../../client");
 const { logger } = require("../../functions/logger");
 
-client.on("ready", async () => {
-    console.log("\n---------------------")
-    logger(`${client.user.tag} is ready`, "success")
-    console.log("---------------------")
+const activities = [
+    { name: "você", type: ActivityType.Watching },
+    { name: "música relaxante", type: ActivityType.Listening },
+    { name: "um jogo divertido", type: ActivityType.Playing },
+    { name: "dicas úteis", type: ActivityType.Streaming, url: "https://www.twitch.tv/kevinterrorista" }
+];
+
+const statuses = ["online", "idle", "dnd"];
+
+function setRandomPresence() {
+    const activity = activities[Math.floor(Math.random() * activities.length)];
+    const status = statuses[Math.floor(Math.random() * statuses.length)];
 
     client.user.setPresence({
-        activities: [
-            {
-                name: "You",
-                type: ActivityType.Watching
-            }
-        ],
-        status: "online"
-    })
-})
+        activities: [activity],
+        status: status
+    });
+
+    logger(`Presença atualizada: ${activity.type} ${activity.name} | Status: ${status}`, "info");
+}
+
+client.on("ready", async () => {
+    console.log("\n---------------------");
+    logger(`${client.user.tag} está pronto!`, "success");
+    console.log("---------------------");
+
+    setRandomPresence();
+
+    setInterval(setRandomPresence, 5 * 60 * 1000);
+});
